2f59a0a315ac5d3cdfc1fa36f8515ef2b78711d0
Shortylabs <shortylabs@gmail.com>
2014-11-21 10:18:57 -0800

    impl IntentService and callback Messenger to get recently added track info

--- app/src/main/java/com/shortylabs/fmarecentlyadded/RecentlyAddedListFragment.java
+++ app/src/main/java/com/shortylabs/fmarecentlyadded/RecentlyAddedListFragment.java
@@ -1,15 +1,23 @@
 package com.shortylabs.fmarecentlyadded;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.shortylabs.fmarecentlyadded.model.RecentlyAddedTrack;
+import com.shortylabs.fmarecentlyadded.service.RecentlyAddedService;
 
-import java.util.ArrayList;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.ref.WeakReference;
 import java.util.List;
 
 /**
@@ -17,40 +25,109 @@ import java.util.List;
  */
 public class RecentlyAddedListFragment extends Fragment {
 
+    private static String TAG = RecentlyAddedListFragment.class.getSimpleName();
+
     public RecentlyAddedListFragment() {
     }
+
+    /**
+     * Instantiate the MessengerHandler, passing in the
+     * Activity to be stored as a WeakReference
+     */
+    MessengerHandler handler = new MessengerHandler(this);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_recently_added_list, container, false);
-        ListView list = (ListView)rootView.findViewById(R.id.listView);
+        ViewHolder.listview =  (ListView)rootView.findViewById(R.id.listView);
+        runService();
+
+        return rootView;
+    }
 
 
-        // dummy data
-        List<RecentlyAddedTrack> trackList = new ArrayList<RecentlyAddedTrack>();
-        trackList.add( new RecentlyAddedTrack(1l, "narrow", "Le Matin"));
-        trackList.add( new RecentlyAddedTrack(1l, "Lucie est malade", "Le Matin"));
-        trackList.add( new RecentlyAddedTrack(1l, "She Turns To Dust", "Auto Bonfire"));
-        trackList.add( new RecentlyAddedTrack(1l, "Living Inside My Computer", "Auto Bonfire"));
-        trackList.add( new RecentlyAddedTrack(1l, "I Have A Feeling That I've Been Here Before", "Le Matin"));
-        trackList.add( new RecentlyAddedTrack(1l, "Angry: A Love Song", "Auto Bonfire"));
-        trackList.add( new RecentlyAddedTrack(1l, "Photograph", "Auto Bonfire"));
-        trackList.add( new RecentlyAddedTrack(1l, "I Sleep Fine", "Auto Bonfire"));
-        trackList.add( new RecentlyAddedTrack(1l, "Just Like Meryl Streep", "Auto Bonfire"));
-        trackList.add( new RecentlyAddedTrack(1l, "Agro-Culture", "Auto Bonfire"));
-        trackList.add( new RecentlyAddedTrack(1l, "What Am I Looking For?", "Auto Bonfire"));
-        trackList.add( new RecentlyAddedTrack(1l, "Moved By Joy", "Sondra Sun-Odeon"));
-        trackList.add( new RecentlyAddedTrack(1l, "Oaks", "Sondra Sun-Odeon"));
-        trackList.add( new RecentlyAddedTrack(1l, "Paradise", "Sondra Sun-Odeon"));
-        trackList.add( new RecentlyAddedTrack(1l, "Belonging", "Sondra Sun-Odeon"));
+    public void runService() {
+
+        Intent intent =
+                RecentlyAddedService.makeIntent(getActivity(),
+                        handler);
+
+        getActivity().startService(intent);
+    }
+
+    private void showResults(String json) {
+
+        Log.d(TAG, json);
+        List<RecentlyAddedTrack> trackList;
+        try {
+            trackList = RecentlyAddedTrack.fromJson(new JSONObject(json));
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage());
+            return;
+        }
+
 
         RecentlyAddedListAdapter adapter = new RecentlyAddedListAdapter(getActivity(),
                 trackList);
 
-        list.setAdapter(adapter);
+        ViewHolder.listview.setAdapter(adapter);
 
-
-        return rootView;
     }
+
+
+    /**
+     * This is the handler used for handling messages sent by a
+     * Messenger.
+     */
+    static class MessengerHandler extends Handler {
+
+        // A weak reference to the enclosing class
+        WeakReference<RecentlyAddedListFragment> outerClass;
+
+        /**
+         * A constructor that gets a weak reference to the enclosing class.
+         * We do this to avoid memory leaks during Java Garbage Collection.
+         *
+         * @ see https://groups.google.com/forum/#!msg/android-developers/1aPZXZG6kWk/lIYDavGYn5UJ
+         */
+        public MessengerHandler(RecentlyAddedListFragment outer) {
+            outerClass = new WeakReference<RecentlyAddedListFragment>(outer);
+        }
+
+        // Handle any messages that get sent to this Handler
+        @Override
+        public void handleMessage(Message msg) {
+
+            Log.d(TAG, "handleMessage");
+
+            final RecentlyAddedListFragment recentlyAddedListFragment = outerClass.get();
+
+            // If RecentlyAddedListFragment hasn't been garbage collected
+            // (closed by user), proceed.
+            if (recentlyAddedListFragment != null) {
+
+                // Extract the data from Message, which is in the form
+                // of a Bundle that can be passed across processes.
+                Bundle data = msg.getData();
+
+                // Extract the result from the Bundle.
+                final String jsonResult = data.getString("result");
+                Log.d(TAG, "Result: " + jsonResult);
+                recentlyAddedListFragment.getActivity().runOnUiThread(new Runnable() {
+                    public void run() {
+                        recentlyAddedListFragment.showResults(jsonResult);
+                    }
+                });
+
+
+            }
+        }
+    }
+
+
+    protected static class ViewHolder{
+        protected static ListView listview;
+    }
+
 }
--- app/src/main/java/com/shortylabs/fmarecentlyadded/model/RecentlyAddedTrack.java
+++ app/src/main/java/com/shortylabs/fmarecentlyadded/model/RecentlyAddedTrack.java
@@ -1,18 +1,38 @@
 package com.shortylabs.fmarecentlyadded.model;
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Jeri on 11/20/14.
  */
 public class RecentlyAddedTrack {
 
+    static final private String TAG = RecentlyAddedTrack.class.getSimpleName();
+
     private long trackId;
     private String trackTitle;
     private String artistName;
+    private String trackImageUrl;
+    private String trackUrl;
+    private String trackDuration ;
+    private int trackBitRate;
+    private String albumTitle;
+    private int trackListens;
+    private int trackDownloads;
+    private int trackFavorites;
 
-    public RecentlyAddedTrack(long trackId, String trackTitle, String artistName) {
+    public RecentlyAddedTrack(long trackId, String trackTitle, String artistName, String trackUrl) {
         this.trackId = trackId;
         this.trackTitle = trackTitle;
         this.artistName = artistName;
+        this.trackUrl = trackUrl;
     }
 
     public long getTrackId() {
@@ -37,5 +57,137 @@ public class RecentlyAddedTrack {
 
     public void setArtistName(String artistName) {
         this.artistName = artistName;
+    }
+
+    public String getTrackImageUrl() {
+        return trackImageUrl;
+    }
+
+    public void setTrackImageUrl(String trackImageUrl) {
+        this.trackImageUrl = trackImageUrl;
+    }
+
+    public String getTrackUrl() {
+        return trackUrl;
+    }
+
+    public void setTrackUrl(String trackUrl) {
+        this.trackUrl = trackUrl;
+    }
+
+    public String getTrackDuration() {
+        return trackDuration;
+    }
+
+    public void setTrackDuration(String trackDuration) {
+        this.trackDuration = trackDuration;
+    }
+
+    public int getTrackBitRate() {
+        return trackBitRate;
+    }
+
+    public void setTrackBitRate(int trackBitRate) {
+        this.trackBitRate = trackBitRate;
+    }
+
+    public String getAlbumTitle() {
+        return albumTitle;
+    }
+
+    public void setAlbumTitle(String albumTitle) {
+        this.albumTitle = albumTitle;
+    }
+
+    public int getTrackListens() {
+        return trackListens;
+    }
+
+    public void setTrackListens(int trackListens) {
+        this.trackListens = trackListens;
+    }
+
+    public int getTrackDownloads() {
+        return trackDownloads;
+    }
+
+    public void setTrackDownloads(int trackDownloads) {
+        this.trackDownloads = trackDownloads;
+    }
+
+    public int getTrackFavorites() {
+        return trackFavorites;
+    }
+
+    public void setTrackFavorites(int trackFavorites) {
+        this.trackFavorites = trackFavorites;
+    }
+
+    @Override
+    public String toString() {
+        return "RecentlyAddedTrack{" +
+                "trackId=" + trackId +
+                ", trackTitle='" + trackTitle + '\'' +
+                ", artistName='" + artistName + '\'' +
+                ", trackImageUrl='" + trackImageUrl + '\'' +
+                ", trackUrl='" + trackUrl + '\'' +
+                ", trackDuration='" + trackDuration + '\'' +
+                ", trackBitRate=" + trackBitRate +
+                ", albumTitle='" + albumTitle + '\'' +
+                ", trackListens=" + trackListens +
+                ", trackDownloads=" + trackDownloads +
+                ", trackFavorites=" + trackFavorites +
+                '}';
+    }
+
+    public static List<RecentlyAddedTrack> fromJson(JSONObject jsonObject) {
+
+        if (jsonObject == null) {
+            return null;
+        }
+
+        List<RecentlyAddedTrack> tracks = new ArrayList<RecentlyAddedTrack>();
+
+        JSONArray jsonTracks = jsonObject.optJSONArray("aTracks");
+        if (jsonTracks == null) {
+            return null;
+        }
+
+        for (int i = 0; i < jsonTracks.length(); i++) {
+            JSONObject jsonTrack = jsonTracks.optJSONObject(i);
+            if (jsonTrack == null) {
+                continue;
+            }
+
+
+            long trackId;
+            String trackTitle;
+            String artistName;
+            String trackUrl;
+
+            try {
+                trackId = jsonTrack.getLong("track_id");
+                trackTitle = jsonTrack.getString("track_title");
+                artistName = jsonTrack.getString("artist_name");
+                trackUrl = jsonTrack.getString("track_url");
+            } catch (JSONException e) {
+                Log.e(TAG, "Required field from track data is not available, skipping...");
+                continue;
+            }
+
+            RecentlyAddedTrack rat = new RecentlyAddedTrack(trackId, trackTitle, artistName, trackUrl);
+
+            rat.setTrackImageUrl(jsonTrack.optString("track_image_file"));
+            rat.setTrackDuration(jsonTrack.optString("track_duration"));
+            rat.setTrackBitRate(jsonTrack.optInt("track_bit_rate"));
+            rat.setAlbumTitle(jsonTrack.optString("album_title"));
+            rat.setTrackListens(jsonTrack.optInt("track_listens"));
+            rat.setTrackDownloads(jsonTrack.optInt("track_downloads"));
+            rat.setTrackFavorites(jsonTrack.optInt("track_favorites"));
+            tracks.add(rat);
+
+        }
+
+        return tracks;
     }
 }
